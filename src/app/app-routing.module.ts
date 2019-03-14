56c72fde07d5fa8cfb5edbcf75f6bfc9f7e849eb
Carlton.Henry <carlton.alvin.henry@gmail.com>
2019-03-14 19:17:47 -0400

    GOof

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainpageComponent} from './home/pages/mainpage/mainpage.component';
+import {UserComponent} from './home/pages/user/user.component';
+import {LoginComponent} from './home/pages/login/login.component';
 
 const routes: Routes = [
-  {path: '', component: MainpageComponent, data: {title: 'Home'}},
+  {path: '', component: MainpageComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
