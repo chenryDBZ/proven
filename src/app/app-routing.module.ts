ce35a7a9d19a94f064a4f8dcbaf515b3b5b735ff
Carlton.Henry <carlton.alvin.henry@gmail.com>
2019-03-14 18:13:01 -0400

    GOof

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainpageComponent} from './home/pages/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageComponent, data: {title: 'Home'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MainpageComponent} from './home/pages/mainpage/mainpage.component';
+import {LoginComponent} from './home/pages/login/login.component';
+import {UserComponent} from './home/pages/user/user.component';
+import {HomeRoutingModule} from './home/home-routing.module';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {FooterComponent} from './shared/components/footer/footer.component';
+import {ButtonsComponent} from './shared/components/buttons/buttons.component';
+import {NavbarComponent} from './shared/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainpageComponent,
+        LoginComponent,
+        UserComponent,
+        FooterComponent,
+        NavbarComponent,
+        ButtonsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HomeRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        AppRoutingModule,
+        NgbModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
